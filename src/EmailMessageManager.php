eeb0f0940a18ef7dcdec3a80724e742f28656f71
upchuk <danny@webomelette.com>
2020-07-10 16:03:46 +0200

    Rendering and sending mails with the manager.

--- src/EmailMessageManager.php
+++ src/EmailMessageManager.php
@@ -4,7 +4,13 @@ namespace Drupal\email_messages;
 
 use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Extension\ThemeHandlerInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Mail\MailManagerInterface;
+use Drupal\Core\Render\RenderContext;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Theme\ThemeInitializationInterface;
+use Drupal\Core\Theme\ThemeManagerInterface;
 
 /**
  * Manages the email messages.
@@ -22,14 +28,49 @@ class EmailMessageManager {
   protected $languageManager;
 
   /**
+   * @var \Drupal\Core\Mail\MailManagerInterface
+   */
+  protected $mailManager;
+
+  /**
+   * @var \Drupal\Core\Theme\ThemeManagerInterface
+   */
+  protected $themeManager;
+
+  /**
+   * @var \Drupal\Core\Theme\ThemeInitializationInterface
+   */
+  protected $themeInitialization;
+
+  /**
+   * @var \Drupal\Core\Extension\ThemeHandlerInterface
+   */
+  protected $themeHandler;
+
+  /**
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
    * EmailMessageManager constructor.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
    * @param \Drupal\Core\Language\LanguageManagerInterface $languageManager
+   * @param \Drupal\Core\Mail\MailManagerInterface $mailManager
+   * @param \Drupal\Core\Theme\ThemeManagerInterface $themeManager
+   * @param \Drupal\Core\Theme\ThemeInitializationInterface $themeInitialization
+   * @param \Drupal\Core\Extension\ThemeHandlerInterface $themeHandler
+   * @param \Drupal\Core\Render\RendererInterface $renderer
    */
-  public function __construct(EntityTypeManagerInterface $entityTypeManager, LanguageManagerInterface $languageManager) {
+  public function __construct(EntityTypeManagerInterface $entityTypeManager, LanguageManagerInterface $languageManager, MailManagerInterface $mailManager, ThemeManagerInterface $themeManager, ThemeInitializationInterface $themeInitialization, ThemeHandlerInterface $themeHandler, RendererInterface $renderer) {
     $this->entityTypeManager = $entityTypeManager;
     $this->languageManager = $languageManager;
+    $this->mailManager = $mailManager;
+    $this->themeManager = $themeManager;
+    $this->themeInitialization = $themeInitialization;
+    $this->themeHandler = $themeHandler;
+    $this->renderer = $renderer;
   }
 
   /**
@@ -70,5 +111,62 @@ class EmailMessageManager {
     $message['value'] = $markup->__toString();
     $entity->set('message', $message);
     return $entity;
+  }
+
+  /**
+   * Mails the message.
+   *
+   * @see \Drupal\Core\Mail\MailManagerInterface::mail()
+   */
+  public function mailMessage(EmailMessageInterface $message, $to, $params = [], $module = NULL, $key = NULL, $reply = NULL, $send = TRUE) {
+    if (!$module) {
+      $module = 'email_messages';
+    }
+
+    if (!$key) {
+      $key = 'notification';
+    }
+
+    if (!isset($params['subject'])) {
+      $params['subject'] = $message->getSubject();
+    }
+
+    if (!isset($params['message'])) {
+      $params['message'] = [
+        '#type' => 'processed_text',
+        '#text' => $message->getMessage()['value'],
+        '#format' => $message->getMessage()['format'],
+      ];
+    }
+
+    $active_theme = $this->setRenderTheme();
+    $params['message'] = $this->renderer->executeInRenderContext(new RenderContext(), function () use ($params) {
+      return $this->renderer->render($params['message']);
+    });
+    $this->restoreDefaultTheme($active_theme);
+
+    return $this->mailManager->mail($module, $key, $to, $message->language()->getId(), $params, $reply, $send);
+  }
+
+  /**
+   * Sets the render theme and returns the original.
+   *
+   * @return \Drupal\Core\Theme\ActiveTheme
+   * @throws \Drupal\Core\Theme\MissingThemeDependencyException
+   */
+  protected function setRenderTheme() {
+    $active_theme = $this->themeManager->getActiveTheme();
+    $frontend_theme = $this->themeInitialization->getActiveThemeByName($this->themeHandler->getDefault());
+    $this->themeManager->setActiveTheme($frontend_theme);
+    return $active_theme;
+  }
+
+  /**
+   * Restores the original theme.
+   *
+   * @param $active_theme
+   */
+  protected function restoreDefaultTheme($active_theme) {
+    $this->themeManager->setActiveTheme($active_theme);
   }
 }
